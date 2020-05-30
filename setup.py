355ff20a66c637ecc9c2dac452a81f5386955bc7
chetannitk <chetanknmiet@gmail.com>
2020-05-31 01:02:35 +0530

    Beer Data science repository creation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Beer Data Science exploratory data analysis.',
+    author='Chetan Chauhan',
+    license='',
+)
